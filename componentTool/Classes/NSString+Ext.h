dd0542efdf1450ce07871d02558c6537780fca42
孙永帅 <sunyongshuai@192.168.0.100>
2019-07-23 10:44:01 +0800

    no message

--- componentTool/Classes/NSString+Ext.h
+++ componentTool/Classes/NSString+Ext.h
@@ -11,22 +11,7 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSString (Ext)
-//过滤表情
-- (NSString *)emojizedString;
-+ (NSString *)emojizedStringWithString:(NSString *)text;
-+ (BOOL)stringContainsEmoji:(NSString *)string;
-+ (NSString *)disable_emoji:(NSString *)text;
-
-/**
- * 获取设备名称
- */
-+ (NSString *)getDeviceName;
-+(CGFloat) heightForString:(NSString *)value fontSize:(CGFloat)fontSize andWidth:(CGFloat)width;
--(CGSize)sizeWithFont:(UIFont *)font maxSize:(CGSize)maxSize;
--(BOOL)hasString:(NSString *)str;
-+ (BOOL)isSIMInstalled;//能否打电话
-//model解析为json串
-- (NSString *)returnSaveModelJsonString:(id)model;
+-(void)test;
 @end
 
 NS_ASSUME_NONNULL_END
